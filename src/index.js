5fd1a6a96fd03357bdf0b786b83e5dab48bcbb4e
oksanalyakhova <o.v.lyakhova@gmail.com>
2021-01-12 10:04:42 +0200

    fixed sw cache

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('./sw.js')
       .then(reg => console.log('SW registered!', reg))
       .catch(err => console.log('Boo!', err));
       })
